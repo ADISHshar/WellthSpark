aff0851560cef158400202e92780d9ccdf12ce34
ADISHshar <141936099+ADISHshar@users.noreply.github.com>
2023-11-30 17:07:05 +0530

    Add files via upload

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<simple_animation_progress_bar/SimpleAnimationProgressBarPlugin.h>)
+#import <simple_animation_progress_bar/SimpleAnimationProgressBarPlugin.h>
+#else
+@import simple_animation_progress_bar;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SimpleAnimationProgressBarPlugin registerWithRegistrar:[registry registrarForPlugin:@"SimpleAnimationProgressBarPlugin"]];
+}
+
+@end
